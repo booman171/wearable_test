d975f419841ff6c0ac88cec94732e9b9f898fe97
Jenario Johnson <jenario171@yahoo.com>
2020-01-19 18:51:38 -0500

    Update record.py

--- record.py
+++ record.py
@@ -3,7 +3,7 @@ from time import sleep
 camera = picamera.PiCamera()
 while true:
 	camera.start_preview()
-	camera.start_recording(‘recorded.h264’)
+	camera.start_recording('my_video.h264')
 	camera.wait_recording(10)
 	camera.stop_recording()
 	camera.stop_preview()
