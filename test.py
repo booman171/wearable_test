7e20bbb7cd96a2b1b272bd5f868c7752a45d083e
Jenario Johnson <jenario171@yahoo.com>
2020-01-19 00:03:03 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+import picamera
+import time
+import board
+import busio
+import adafruit_lsm9ds1
+
+
+# I2C connection:
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.start_recording('my_video.h264')
+
+while True:
+
+    # Read acceleration, magnetometer, gyroscope, temperature.
+    accel_x, accel_y, accel_z = sensor.acceleration
+    mag_x, mag_y, mag_z = sensor.magnetic
+    gyro_x, gyro_y, gyro_z = sensor.gyro
+    temp = sensor.temperature
+	camera.annotate_text = str(accel_x)
+    # Print values.
+    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
+    print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
+    print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
+    print('Temperature: {0:0.3f}C'.format(temp))
+    # Delay for a second.
+    time.sleep(1.0)
+
+
+camera.stop_recording()
