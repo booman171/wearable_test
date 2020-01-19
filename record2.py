fb04087b3611b3d99c5be109e83ce11124405801
Jenario Johnson <jenario171@yahoo.com>
2020-01-18 23:46:19 -0500

    Update record2.py

--- record2.py
+++ record2.py
@@ -14,30 +14,30 @@ var = 1
 while var == 1 :
     camera = picamera.PiCamera()
     try:
-        #IMU
-	accel_x, accel_y, accel_z = sensor.acceleration
-	mag_x, mag_y, mag_z = sensor.magnetic
-	gyro_x, gyro_y, gyro_z = sensor.gyro
-	temp = sensor.temperature
-	print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
-	print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
-	print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
-	print('Temperature: {0:0.3f}C'.format(temp))
+		#IMU
+		accel_x, accel_y, accel_z = sensor.acceleration
+		mag_x, mag_y, mag_z = sensor.magnetic
+		gyro_x, gyro_y, gyro_z = sensor.gyro
+		temp = sensor.temperature
+		print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
+		print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
+		print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
+		print('Temperature: {0:0.3f}C'.format(temp))
 
-	#Video
-        fileTime = str(time.time())
-        fileName = "video.h264"
-	print("Newest File Is: " + fileTime)
-        camera.resolution = (1280, 720)
+		#Video
+		fileTime = str(time.time())
+		fileName = "video.h264"
+		print("Newest File Is: " + fileTime)
+		camera.resolution = (1280, 720)
 
-        #camera.sensor_mode = 1 #closer to 1 lighter video is
-        camera.start_recording(fileName)
-        start = dt.datetime.now()
+		#camera.sensor_mode = 1 #closer to 1 lighter video is
+		camera.start_recording(fileName)
+		start = dt.datetime.now()
 
-	while (dt.datetime.now() - start).seconds < 600 : #600 10 mins
-            camera.annotate_text = dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-            camera.wait_recording(0.2)
-        camera.stop_recording()
+		while (dt.datetime.now() - start).seconds < 600 : #600 10 mins
+		    camera.annotate_text = dt.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+		    camera.wait_recording(0.2)
+		camera.stop_recording()
 
     finally:
         camera.close()
