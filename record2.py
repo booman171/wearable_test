d3d2501d0e0ef0f2144c8358ef4a0804beabd65a
jjohnson3029 <jenario171@yahoo.com>
2020-01-18 23:42:45 -0500

    update

--- record2.py
+++ record2.py
@@ -1,19 +1,30 @@
 import picamera,os,time
 from time import sleep
 import datetime as dt
+import board
+import busio
+import adafruit_lsm9ds1
+
+#I@C Connection:
+i2c = busio,I2C(board.SCL, board.SDA)
+sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
 var = 1
 
 while var == 1 :
     camera = picamera.PiCamera()
     try:
-        print()
-        #list = os.listdir("/home/pi/Desktop/Videos")
-        #list.sort()
-        #print(list)
-        #os.remove("/home/pi/Desktop/Videos/"+list[0])
-        #list1 = os.listdir("/home/pi/Desktop/Videos")
-        #print(list1)
+        #IMU
+	accel_x, accel_y, accel_z = sensor.acceleration
+	mag_x, mag_y, mag_z = sensor.magnetic
+	gyro_x, gyro_y, gyro_z = sensor.gyro
+	temp = sensor.temperature
+	print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
+	print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
+	print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
+	print('Temperature: {0:0.3f}C'.format(temp))
+
+	#Video
         fileTime = str(time.time())
         fileName = "video.h264"
 	print("Newest File Is: " + fileTime)
