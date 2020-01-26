924fa7e2d215d7dd9691faa9495aafef1d67fa9b
jjohnson3029 <jenario171@yahoo.com>
2020-01-25 19:10:28 -0500

    update

--- test5.py
+++ test5.py
@@ -13,6 +13,9 @@ import busio
 import adafruit_sgp30
 import adafruit_lsm9ds1
 import csv
+from datetime import datetime
+
+
 
 
 f = open("accel.csv", "w", newline="")
@@ -56,7 +59,7 @@ while(True):
     gyro_x, gyro_y, gyro_z = sensor.gyro
     temp = sensor.temperature
 
-    c.writerow([accel_x, accel_y])
+    c.writerow([datetime.now(), accel_x, accel_y, accel_z, temp])
     # Print values.
     print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
     print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
@@ -67,7 +70,7 @@ while(True):
     # Draw framerate in corner of frame
     #print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
     
-    cv2.putText(frame,str(accel_x),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(51, 51, 0),2,cv2.LINE_AA)
+    cv2.putText(frame,str(datetime.now()),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(51, 51, 0),2,cv2.LINE_AA)
     #cv2.putText(frame,str(sgp30.eCO2),(30,90),cv2.FONT_HERSHEY_SIMPLEX,1,(51, 51, 0),2,cv2.LINE_AA)
     #print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
     #time.sleep(1)
