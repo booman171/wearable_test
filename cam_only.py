e4d8379c789c3df61b553e478e2e8dbdb78c2fc5
unknown <jenario171@yahoo.com>
2020-02-29 20:42:15 -0500

    update

--- cam_only.py
+++ cam_only.py
@@ -165,7 +165,7 @@ while(True):
         cv2.putText(ov,"Snap",(255,175),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,0),1,cv2.LINE_AA)
         cv2.putText(ov,"Menu",(255,230),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,0),1,cv2.LINE_AA)
         if recording == True:
-            cv2.rectangle(ov,(330,01),(333,13),(51,51,0),cv2.FILLED)
+            cv2.rectangle(ov,(330,10),(333,13),(51,51,0),cv2.FILLED)
 
 
         # Combine overlay to frame, apply transparency
