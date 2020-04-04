625e7b80059249daa926038e0c31fad98ae0d2b0
jjohnson3029 <jenario171@yahoo.com>
2020-04-04 18:25:10 -0400

    update

--- bpm.py
+++ bpm.py
@@ -3,8 +3,8 @@ import time
 class BPM:
     def __init__(self):
         #self.ecg = ecg
-        self.max_buffer = 100
-        self.prevData = [None] * MAX_BUFFER
+        self.MAX_BUFFER = 100
+        self.prevData = [None] * self.MAX_BUFFER
         self.sumData=0
         self.maxData=0
         self.avgData=0
@@ -15,20 +15,21 @@ class BPM:
         self.millistimer=time.time()
         self.frequency=0
         self.beatspermin=0
+
     def getBPM(self):
         return self.beatspermin
 
-    def freqDetec(self){
+    def freqDetec(self):
         if(self.countData == self.MAX_BUFFER):
-            if(self.prevData{self.roundrobin] < self.avgData*1.5 and self.newData >= self.avgData*1.5:
+            if(self.prevData[self.roundrobin] < self.avgData*1.5 and self.newData >= self.avgData*1.5):
                 self.period = time.time()-self.millistimer
                 self.millistimer = time.time()
                 self.maxData = 0
-        self.roundrobin++
+        self.roundrobin += 1
         if(self.roundrobin >= self.MAX_BUFFER):
             self.roundrobin=0
         if(self.countData<self.MAX_BUFFER):
-            self.countData++
+            self.countData += 1
             self.sumData+=self.newData
         else:
             self.sumData+=self.newData-self.prevData[self.roundrobin];
@@ -36,13 +37,14 @@ class BPM:
         if(self.newData>self.maxData):
             self.maxData = self.newData
         self.prevData[self.roundrobin] = self.newData; #store previous value
+        print("hjh" + str(self.maxData))
 
     def calcBPM(self, ecg):
         self.newData = ecg
         self.freqDetec();
         if (self.period!=self.lastperiod):
-            self.frequency = 1000/float(self.period);//timer rate/period
-            if (self.frequency*60 > 20 && self.frequency*60 < 200): # supress unrealistic Data
+            self.frequency = 1000/float(self.period); #timer rate/period
+            if (self.frequency*60 > 20 and self.frequency*60 < 200): # supress unrealistic Data
                 self.beatspermin=self.frequency*60;
                 self.lastperiod=self.period;
-        time.sleep(5)
+        time.sleep(0.01)
--- sensor_serial.py
+++ sensor_serial.py
@@ -2,9 +2,9 @@ import threading
 import time
 import serial
 #import heartpy as hp
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from bpm import BPM
-import numpy as np
+#import numpy as np
 import csv
 
 class Sensorserial:
