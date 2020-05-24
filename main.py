8092e82ac11f7da857e5f178fa613a6c8bd880de
jjohnson3029 <jenario171@yahoo.com>
2020-05-23 23:55:58 -0400

    update

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ from overlay import overlay_transparent
 from firebase import firebase
 import glob
 import bluetooth
+from get_video import VideoGet
+from flask import Flask, render_template, Response
+import argparse
 
 #from sensor_serial import read_from_port
 #from metawear import MWBoard
@@ -40,6 +43,15 @@ text = font.render("Hello, World", True, (0, 128, 0))
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+ds_factor = 0.6
+
 
 #host = ""
 #port = 1    # Raspberry Pi uses port 1 for Bluetooth Communication
@@ -85,6 +97,94 @@ thermometer = pygame.image.load('therm.png')
 thermometer = pygame.transform.rotozoom(thermometer, 0, 0.08)
 
 firebase = firebase.FirebaseApplication('https://wear1-38901.firebaseio.com/')
+
+def index():
+        # return the rendered template
+        return render_template("index.html")
+		
+
+def detect_motion(frameCount):
+        # grab global references to the video stream, output frame, and
+        # lock variables
+        global vs, outputFrame, lock
+
+        # initialize the motion detector and the total number of frames
+        # read thus far
+        md = SingleMotionDetector(accumWeight=0.1)
+        total = 0
+
+        # loop over frames from the video stream
+        while True:
+                # read the next frame from the video stream, resize it,
+                # convert the frame to grayscale, and blur it
+                frame = vs.read()
+                frame = imutils.resize(frame, width=400)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+                # grab the current timestamp and draw it on the frame
+                timestamp = datetime.datetime.now()
+                cv2.putText(frame, timestamp.strftime(
+                        "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+                # if the total number of frames has reached a sufficient
+                # number to construct a reasonable background model, then
+                # continue to process the frame
+                if total > frameCount:
+                        # detect motion in the image
+                        motion = md.detect(gray)
+
+                        # cehck to see if motion was found in the frame
+                        if motion is not None:
+                                # unpack the tuple and draw the box surrounding the
+                                # "motion area" on the output frame
+                                (thresh, (minX, minY, maxX, maxY)) = motion
+                                cv2.rectangle(frame, (minX, minY), (maxX, maxY),
+                                        (0, 0, 255), 2)
+
+                # update the background model and increment the total number
+                # of frames read thus far
+                md.update(gray)
+                total += 1
+
+                # acquire the lock, set the output frame, and release the
+                # lock
+                with lock:
+                        outputFrame = frame.copy()
+
+
+def generate():
+        # grab global references to the output frame and lock variables
+        global outputFrame, lock
+
+        # loop over frames from the output stream
+        while True:
+                # wait until the lock is acquired
+                with lock:
+                        # check if the output frame is available, otherwise skip
+                        # the iteration of the loop
+                        if outputFrame is None:
+                                continue
+
+                        # encode the frame in JPEG format
+                        (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+                        # ensure the frame was successfully encoded
+                        if not flag:
+                                continue
+
+                # yield the output frame in the byte format
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                        bytearray(encodedImage) + b'\r\n')
+
+
+@app.route("/video_feed")
+def video_feed():
+        # return the response generated along with the specific media
+        # type (mime type)
+        return Response(generate(),
+                mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 # Define method for overlaying trasnparent images
 def handle_data(data):
@@ -205,6 +305,24 @@ menu_items = ["J", "Play/P", "Next", "Prev", "Vol+", "Vol-", "voice", "Back"]
 time.sleep(1)
 
 # Video processing
+#global frame
+def threadVideoGet(source=0):
+        '''
+        Thread for getting video from get_video
+        '''
+        video_getter = VideoGet(source).start()
+        while True:
+                if (cv2.waitKey(1) == ord("q")) or video_getter.stopped:
+                        video_getter.stop()
+                        break
+                frame = video_getter.frame
+                frame=cv2.resize(frame,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+                gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                #cv2.imshow("Video", frame)
+                jpg = jpeg.tobytes()
+                yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpg + b'\r\n\r\n')
+
 while(True):
     if main == True:
         now = datetime.now()
