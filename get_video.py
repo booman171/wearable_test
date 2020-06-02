da7aabd91492fb1d8ab51d9d3920dcb78f890058
jjohnson3029 <jenario171@yahoo.com>
2020-06-01 20:51:30 -0400

    update

--- get_video.py
+++ get_video.py
@@ -19,7 +19,7 @@ class VideoGet:
 	def get(self):
 		self.timestr = time.strftime("%Y%m%d-%H%M%S")
 		self.filename = 'video' + self.timestr + '.avi' # .avi .mp4
-		self.fps = 5.0
+		self.fps = 30.0
 		self.video_writer = cv2.VideoWriter_fourcc('M','J','P','G')
 		self.video_out = cv2.VideoWriter(self.filename, self.video_writer, self.fps, (640, 480))
 
